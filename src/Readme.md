2b81011b4d6eaa78298743d8fbed04b9deebc38b
Adrian Bucur <adrian.bucur@yap.com>
2022-12-09 06:47:03 +0200

    first commit

---
+++ src/Readme.md
@@ -0,0 +1,3 @@
+Access the application at:
+
+http://localhost:4000/students
---
+++ src/test/java/com/siit/fullAppExample/FullAppExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siit.fullAppExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullAppExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
